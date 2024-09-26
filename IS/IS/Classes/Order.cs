4cd350f336cc503b859764fe4c0d0a8a116d69f7
Vadim Elsukov <elsukov.vadim@gmail.com>
2024-09-26 15:26:06 +0300

    test

---
+++ IS/IS/Classes/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS.Classes
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime FinishedAt { get; set; }
+        public string Status { get; set; } // Статус заказа (например: "Ожидание", "В процессе", "Завершен")
+        public string Description { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string PaymentMethod { get; set; } // Например, "Наличные", "Карта"
+        public string Device { get; set; } // Устройство, предоставленное на ремонт
+
+        // Поле для хранения выбранных услуг
+        public List<Service> Services { get; set; } = new List<Service>();
+    }
+
+}
---
+++ IS/IS/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace IS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.profileButton = new System.Windows.Forms.Button();
+            this.requestButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // profileButton
+            // 
+            this.profileButton.Location = new System.Drawing.Point(167, 126);
+            this.profileButton.Name = "profileButton";
+            this.profileButton.Size = new System.Drawing.Size(155, 43);
+            this.profileButton.TabIndex = 1;
+            this.profileButton.Text = "Профиль";
+            this.profileButton.UseVisualStyleBackColor = true;
+            this.profileButton.Click += new System.EventHandler(this.profileButton_Click);
+            // 
+            // requestButton
+            // 
+            this.requestButton.Location = new System.Drawing.Point(167, 192);
+            this.requestButton.Name = "requestButton";
+            this.requestButton.Size = new System.Drawing.Size(155, 65);
+            this.requestButton.TabIndex = 2;
+            this.requestButton.Text = "Оставить заявку на ремнот";
+            this.requestButton.UseVisualStyleBackColor = true;
+            this.requestButton.Click += new System.EventHandler(this.requestButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 483);
+            this.Controls.Add(this.requestButton);
+            this.Controls.Add(this.profileButton);
+            this.Name = "Form1";
+            this.Text = "Сервис ремонта компьютерной техники";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button profileButton;
+        private System.Windows.Forms.Button requestButton;
+    }
+}
+
